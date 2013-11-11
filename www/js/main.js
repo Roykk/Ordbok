602a7a2ce580e7517a424fa9c9e10d7b27f68fb3
Roy Kjellid <roy.kjellid@gmail.com>
2013-11-11 22:11:27 +0100

    javascript updated

--- www/js/main.js
+++ www/js/main.js
@@ -1,38 +1,55 @@
 $(function(){
 	$("#searchForm").submit(function(event){
 		event.preventDefault();
-		$.ajax({
-   			url: this.action,
-   			cache: false,
-  			async: false,
-  			data:{ OPP: this.OPP.value },
-  			dataType: "html",
-  			success: function(data){
-     			html = data;
-     			//hvis bokmål er checked
-     			$("#bokmaal").html($(html).find("#byttutBM").html());
+    var input = validateInput(this.searchText.value);
+    if(input!=null){
+      input = String(input);
+  		$.ajax({
+     			url: getActionUrl(),
+          type:"get",
+     			cache: false,
+    			async: false,
+    			data:{OPP:input},
+    			dataType: "html",
+    			success: function(data){
+       			html = data;
+       			//hvis bokmål er checked
+       			$("#bokmaal").html($(html).find("#byttutBM").html());
 
-     			//hvis nynorsk er checked
-     			$("#nynorsk").html($(html).find("#byttutNN").html());
+       			//hvis nynorsk er checked
+       			$("#nynorsk").html($(html).find("#byttutNN").html());
 
 
-     			alert();//ordforklaring span.tydningC.kompakt))
+       			//ordforklaring span.tydningC.kompakt))
 
-				alert(); //selve ordet a.tiptip, om det er bokmaal jQuery('html').find('a.tiptip')[0].onclick.toString().indexOf('bokmaal')
+    				//selve ordet a.tiptip, om det er bokmaal jQuery('html').find('a.tiptip')[0].onclick.toString().indexOf('bokmaal')
 
-   			}
-		});
+     			}
+  		});
+    }else{
+      alert('Ditt søk inneholder et eller flere ugyldige tegn.');
+    }
 	});
+
+  $("#searchIcon").click(function(event){
+    $("form[name='searchForm']").submit();
+  });
+
 });
 
-		// $.get( this.action, { OPP: this.OPP.value }, "html" )
-         //        .success(function(data){
-           //       	alert(data);
-                 	
-           //       	var tableHtml =  $(data).find("body");
-           //      	alert(tableHtml);
-           //      	alert(tableHtml.html());
-           //         $('#result').html(tableHtml.html()) ;
-           //      }).error(function(response,status){
-           //      	alert("fail:" + status);
-           //      }); 
+
+function validateInput(input){
+  if(input!=null){
+    input = input.trim().split(" ",1);
+    var pattern = /^[ÆØÅæøåA-Za-z]*$/i;
+    return input[0].match(pattern);
+  }
+}
+
+function getActionUrl(){
+  return "http://www.vg.no"; 
+  //this is commented out to not spam uioordbok with wrongfull queries
+  //"http://www.nob-ordbok.uio.no/perl/ordbok.cgi"
+}
+
+		
